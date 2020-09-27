a485c2af926d760cb6417bcb4cb3be87b67d3ca7
Riley Post <>
2020-09-28 07:49:29 +1000

    Added template file, style change

---
+++ wordpress/wp-content/themes/bewd-p2/single-architects.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * Template Name: Architects Post
+ * Template Post Type: post, page, product
+ */
+/**
+ * The template for displaying single posts in the architects category
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package BeWD_P2
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', get_post_type() );
+
+			the_post_navigation(
+				array(
+					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'bewd-p2' ) . '</span> <span class="nav-title">%title</span>',
+					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'bewd-p2' ) . '</span> <span class="nav-title">%title</span>',
+				)
+			);
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
